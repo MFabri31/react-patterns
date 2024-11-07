2eca64abe213087934f6406610b9781b6685386f
MFabri31 <medinajose3195@gmail.com>
2024-11-07 19:38:57 -0300

    First commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+function App() {
+  return (
+    <>
+      <main className="container ">
+        <h1
+          style={{ textAlign: "center", padding: "1em 0" }}
+          className="mx-auto"
+        >
+          React Patterns
+        </h1>
+      </main>
+    </>
+  );
+}
+
+export default App;
